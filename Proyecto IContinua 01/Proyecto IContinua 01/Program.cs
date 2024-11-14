1e4a32bfac0360833297d006dffb2ea523bfb465
José Alexander Bautista Gálvez <jsalexchota@gmail.com>
2024-11-14 12:19:38 -0500

    Segunda Actualizacion del Proyecto

--- Proyecto IContinua 01/Proyecto IContinua 01/Program.cs
+++ Proyecto IContinua 01/Proyecto IContinua 01/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mi nombre es Alex");
+
+Console.WriteLine("Mi apellido es: Bautista");
